716487411b328f029f8571516d72eee662e2ce7d
rahul2009suresh <87611919+rahul2009suresh@users.noreply.github.com>
2023-02-20 19:07:37 +0400

    commit-1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import FlightResults from './FlightResults';
 
 function App() {
+  const [fromCity, setFromCity] = useState('');
+  const [toCity, setToCity] = useState('');
+  const [departureDate, setDepartureDate] = useState('');
+  const [returnDate, setReturnDate] = useState('');
+  const [adults, setAdults] = useState(0);
+  const [children, setChildren] = useState(0);
+  const [flightResults, setFlightResults] = useState(null);
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    // Here you can fetch the data from a backend API and update the flightResults state
+    // For demonstration purposes, let's just set some sample data
+    const sampleData = [
+      {
+        airline: 'Rahul Flight Services',
+        flightNumber: 'RF123',
+        departureTime: '8:00 AM',
+        arrivalTime: '10:30 AM',
+        price: 250
+      },
+      {
+        airline: 'Rahul Flight Services',
+        flightNumber: 'RF456',
+        departureTime: '1:00 PM',
+        arrivalTime: '3:30 PM',
+        price: 350
+      }
+    ];
+
+    setFlightResults(sampleData);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Rahul Flight Services</h1>
+      <form onSubmit={handleFormSubmit}>
+        <label>From:</label>
+        <input type="text" value={fromCity} onChange={(e) => setFromCity(e.target.value)} required />
+
+        <label>To:</label>
+        <input type="text" value={toCity} onChange={(e) => setToCity(e.target.value)} required />
+
+        <label>Departure Date:</label>
+        <input type="date" value={departureDate} onChange={(e) => setDepartureDate(e.target.value)} required />
+
+        <label>Return Date:</label>
+        <input type="date" value={returnDate} onChange={(e) => setReturnDate(e.target.value)} />
+
+        <label>Adults:</label>
+        <input type="number" value={adults} onChange={(e) => setAdults(e.target.value)} required />
+
+        <label>Children:</label>
+        <input type="number" value={children} onChange={(e) => setChildren(e.target.value)} />
+
+        <button type="submit">Search Flights</button>
+      </form>
+
+      {flightResults && <FlightResults results={flightResults} />}
     </div>
   );
 }
